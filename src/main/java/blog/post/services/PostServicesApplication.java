c3431547531c40412c9ca2c62f264043263d504d
Pedro Fonseca Nunes <pedrofonunes@gmail.com>
2024-08-19 01:56:40 -0300

    feat: construindo camada de postagens.

---
+++ src/main/java/blog/post/services/PostServicesApplication.java
@@ -0,0 +1,13 @@
+package blog.post.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blog/post/services/PostServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.post.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
